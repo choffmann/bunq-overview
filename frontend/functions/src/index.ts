c57e31614796661984cbc5219ecb112d7f31c7e2
Cedrik Hoffmann <choffmann@progeek.de>
2023-09-10 12:23:40 +0200

    Implement validateFirebaseIdToken.ts

--- frontend/functions/src/index.ts
+++ frontend/functions/src/index.ts
@@ -3,6 +3,7 @@ import * as admin from 'firebase-admin'
 import * as express from 'express'
 import * as bodyParser from "body-parser";
 import {BunqApiContext} from "./BunqApiContext";
+import {validateFirebaseIdToken} from "./validateFirebaseIdToken";
 
 admin.initializeApp({
     databaseURL: "localhost:8080"
@@ -15,7 +16,7 @@ const main = express()
 main.use("/api/v1", app)
 main.use(bodyParser.json())
 main.use(bodyParser.urlencoded({ extended: false }));
-
+app.use(validateFirebaseIdToken)
 
 app.get("/users", async (req, res)  => {
     const sandboxKey = "sandbox_39949d0c10c82768dc69a8831754fa1c9333bde07f336d17e9c31122"
